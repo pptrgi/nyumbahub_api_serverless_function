15d2b3114af956c62ea8d58c22120b5a32e1e62c
pptrgi <pptrgitonga@gmail.com>
2023-08-21 14:53:39 +0300

    Include the frontend URL in CORS and add its link

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,14 +1,11 @@
 const allowedOrigins = [
   "https://nyumbahub.netlify.app",
-  "https://nh-pptrgitonga-gmailcom.vercel.app",
+  "https://nyumbahub.vercel.app",
 ];
 
 const corsOptions = {
   origin: (origin, callback) => {
     if (allowedOrigins.includes(origin) || !origin) {
-      // allow same-origin requests with !origin
-      // that includes https://nyumbahub.netlify.app and its subsequent routes
-
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
